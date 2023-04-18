61bff21b86644a6454bd5417aa985beb10fa201f
Rod MacKenzie <rod.n.mackenzie@gmail.com>
2023-04-18 15:10:28 +0100

    Add project with a Teamcity environment to test the plugin

---
+++ kotlin-script-runner-test/build.gradle.kts
@@ -0,0 +1,25 @@
+
+plugins {
+    id ("io.github.rodm.teamcity-environments")
+}
+
+extra["java11Home"] = project.findProperty("java11.home") ?: "/opt/jdk-11.0.2"
+
+val plugins by configurations.creating
+
+dependencies {
+    plugins (project(path = ":kotlin-script-runner-server", configuration = "plugin"))
+}
+
+teamcity {
+    environments {
+        baseHomeDir = "${projectDir}/servers"
+        baseDataDir = "${projectDir}/data"
+
+        register("TeamCity2022.10") {
+            version = "2022.10.3"
+            javaHome = extra["java11Home"] as String
+            plugins = configurations["plugins"]
+        }
+    }
+}
